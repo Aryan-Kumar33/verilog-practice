11fb2051576264f0bcb9146e37364fe6905605b0
Aryan Kumar <aryanthakur6299@gmail.com>
2026-09-23 20:24:40 +0530

    Add half adder Verilog design and testbench

---
+++ half_adder_tb.v
@@ -0,0 +1,25 @@
+module half_adder_tb;
+
+reg A;
+reg B;
+wire Sum;
+wire Carry;
+
+half_adder uut (
+    .A(A),
+    .B(B),
+    .Sum(Sum),
+    .Carry(Carry)
+);
+
+initial begin
+    $monitor("A=%b B=%b | Sum=%b Carry=%b", A, B, Sum, Carry);
+
+    A=0; B=0;
+    #10 A=0; B=1;
+    #10 A=1; B=0;
+    #10 A=1; B=1;
+    #10 $finish;
+end
+
+endmodule
